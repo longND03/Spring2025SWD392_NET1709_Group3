afc6cb0199cc317909dc1bdcb3c0b40c1b6e62ee
PTPhong-PPL <phongptse183388@fpt.edu.vn>
2025-03-06 13:47:30 +0700

    update order history link on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -128,7 +128,7 @@ const Navbar = () => {
                     Profile
                   </Link>
                   <Link
-                    to="/orders"
+                    to="/orders?tab=orders"
                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                   >
                     My Orders
